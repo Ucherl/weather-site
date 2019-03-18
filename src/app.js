50dfc83eca80ecd1ee6b1c85266e54aa644522fe
kao kao <kaokao@ntu.gg>
2019-03-18 23:40:40 +0800

    set up for heroku

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require('express')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode.js')
 const forcast = require('./utils/forecast.js')
-const app = express();
+
+const app = express()
+const port = process.env.PORT || 3000 
 
 // define paths for express config
 const publicDirectoryPath = path.join(__dirname,'../puclic')
@@ -94,6 +96,6 @@ app.get('*',(req,res)=>{
 })
 
 
-app.listen(3000, ()=>{
-    console.log('server is on on port 3000');
+app.listen(port, ()=>{
+    console.log('server is on on port '+ port);
 })
